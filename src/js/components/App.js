0ee5228a3fef56dd06d4871a623075820716840b
Crystal Wang <crystalmaro@gmail.com>
2019-11-05 01:42:01 +0800

    fix

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-// import { Provider } from 'react-redux'
 import List from './List';
 import Profile from './Profile';
-
-// const App = () => (
-// 	<>
-// 		<List />
-// 		<Profile />
-// 	</>
-// 	// <Router>
-// 	// 	<Route exact path="/" render={() => <Redirect to="/heroes" />} />
-// 	// 	<Route path="/heroes" component={List} />
-// 	// 	<Route path="/heroes/:heroId" component={Profile} />
-// 	// </Router>
-// );
 
 export class App extends Component {
 	render() {
 		return (
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<Route exact path="/" render={() => <Redirect to="/heroes" />} />
 				<Route path="/heroes" component={List} />
 				<Route path={`/heroes/${this.props.currentHeroID}`} component={Profile} />
